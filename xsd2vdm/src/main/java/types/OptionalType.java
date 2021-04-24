681249339605fe682ce9629f8dd335236b4a9db7
nick_battle <nick.battle@gmail.com>
2021-04-24 11:17:44 +0100

    Make OptionalType isOptional :)

--- xsd2vdm/src/main/java/types/OptionalType.java
+++ xsd2vdm/src/main/java/types/OptionalType.java
@@ -50,6 +50,12 @@ public class OptionalType extends Type
 	}
 
 	@Override
+	public boolean isOptional()
+	{
+		return true;
+	}
+	
+	@Override
 	public String signature()
 	{
 		return (type instanceof OptionalType) ?
