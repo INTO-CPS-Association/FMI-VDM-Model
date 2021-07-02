312afe46d5334a494b155c6490631cd4d29a1754
nick_battle <nick.battle@gmail.com>
2021-07-02 08:45:47 +0100

    Fix for any

--- xsd2vdm/src/main/java/xsd2vdm/XSDConverter_v11.java
+++ xsd2vdm/src/main/java/xsd2vdm/XSDConverter_v11.java
@@ -1103,7 +1103,7 @@ public class XSDConverter_v11 extends XSDConverter
 				default:
 					if (child.isType("xs:any"))
 					{
-						convertAny(child);
+						fields.addAll(convertAny(child));
 					}
 					else if (isChoiceSequence(child))
 					{
