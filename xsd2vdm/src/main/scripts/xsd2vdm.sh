353e014f9918e0454c56baa44f24c746478c2fdb
nick_battle <nick.battle@gmail.com>
2021-08-31 20:35:31 +0100

    Correct VERSION in xsd2vdm.sh

--- xsd2vdm/src/main/scripts/xsd2vdm.sh
+++ xsd2vdm/src/main/scripts/xsd2vdm.sh
@@ -3,7 +3,7 @@
 # Script to launch the installed XSD2VDM jar.
 #
 
-VERSION=1.0.3-SNAPSHOT
+VERSION=1.1.1-SNAPSHOT
 JAR=$HOME/.m2/repository/org/into-cps/vdmcheck/xsd2vdm/$VERSION/xsd2vdm-$VERSION.jar
 
 exec java -jar $JAR $@
