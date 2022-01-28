9d75915db24a9f8a8d036a815bdbc4fafe4589cf
nick_battle <nick.battle@gmail.com>
2022-01-28 15:43:17 +0000

    Rename vendor annotations

---
+++ renumber.sh
@@ -0,0 +1,17 @@
+# Renumber fmi3
+fmi3/static-model/BuildConfiguration_2.2.2.vdmsl
+fmi3/static-model/CoSimulation_4.2.1.vdmsl
+fmi3/static-model/DefaultExperiment_2.2.6.vdmsl
+fmi3/static-model/FMI3Schema.vdmsl
+fmi3/static-model/FMIModelDescription_2.2.1.vdmsl
+fmi3/static-model/GraphicalRepresentation_2.2.8.vdmsl
+fmi3/static-model/LogCategories_2.2.5.vdmsl
+fmi3/static-model/ModelExchange_4.3.1.vdmsl
+fmi3/static-model/ModelStructure_2.2.11.vdmsl
+fmi3/static-model/ModelVariables_2.2.10.vdmsl
+fmi3/static-model/Terminals_2.2.7.vdmsl
+fmi3/static-model/TypeDefinitions_2.2.4.vdmsl
+fmi3/static-model/UnitDefinitions_2.2.3.vdmsl
+fmi3/static-model/Validation.vdmsl
+fmi3/static-model/VariableNaming_2.2.12.vdmsl
+fmi3/static-model/VendorAnnotations_2.2.9.vdmsl
