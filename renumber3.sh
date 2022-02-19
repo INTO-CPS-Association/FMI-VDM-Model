df544eb66ef4ae0d83ee3ecdf6bb0a14a08f77bb
nick_battle <nick.battle@gmail.com>
2022-02-19 17:20:52 +0000

    Added some Clock validation

--- renumber3.sh
+++ renumber3.sh
@@ -7,19 +7,20 @@ function renumber()
 }
 
 
-renumber fmi3/static-model/BuildConfiguration_2.2.2.vdmsl
-renumber fmi3/static-model/CoSimulation_4.2.1.vdmsl
-renumber fmi3/static-model/DefaultExperiment_2.2.6.vdmsl
-renumber fmi3/static-model/FMI3Schema.vdmsl
-renumber fmi3/static-model/FMIModelDescription_2.2.1.vdmsl
-renumber fmi3/static-model/GraphicalRepresentation_2.2.8.vdmsl
-renumber fmi3/static-model/LogCategories_2.2.5.vdmsl
-renumber fmi3/static-model/ModelExchange_4.3.1.vdmsl
-renumber fmi3/static-model/ModelStructure_2.2.11.vdmsl
-renumber fmi3/static-model/ModelVariables_2.2.10.vdmsl
-renumber fmi3/static-model/Terminals_2.2.7.vdmsl
-renumber fmi3/static-model/TypeDefinitions_2.2.4.vdmsl
-renumber fmi3/static-model/UnitDefinitions_2.2.3.vdmsl
+renumber fmi3/static-model/Annotations.vdmsl
+renumber fmi3/static-model/BuildConfiguration.vdmsl
+renumber fmi3/static-model/CoSimulation.vdmsl
+renumber fmi3/static-model/DefaultExperiment.vdmsl
+renumber fmi3/static-model/FMIModelDescription.vdmsl
+renumber fmi3/static-model/GraphicalRepresentation.vdmsl
+renumber fmi3/static-model/LogCategories.vdmsl
+renumber fmi3/static-model/Misc.vdmsl
+renumber fmi3/static-model/ModelExchange.vdmsl
+renumber fmi3/static-model/ModelStructure.vdmsl
+renumber fmi3/static-model/ModelVariables.vdmsl
+renumber fmi3/static-model/ScheduledExecution.vdmsl
+renumber fmi3/static-model/Terminals.vdmsl
+renumber fmi3/static-model/TypeDefinitions.vdmsl
+renumber fmi3/static-model/UnitDefinitions.vdmsl
 renumber fmi3/static-model/Validation.vdmsl
-renumber fmi3/static-model/VariableNaming_2.2.12.vdmsl
-renumber fmi3/static-model/VendorAnnotations_2.2.9.vdmsl
+renumber fmi3/static-model/VariableNaming.vdmsl
