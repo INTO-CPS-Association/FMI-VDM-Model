3890061d93a58de6508acbd63a49f9a0c42ce1d9
nick_battle <nick.battle@gmail.com>
2022-04-19 11:11:16 +0100

    Update tests, script and POM for new rules version

--- fmi3/vdmcheck/src/main/scripts/VDMCheck3.sh
+++ fmi3/vdmcheck/src/main/scripts/VDMCheck3.sh
@@ -172,10 +172,15 @@ function check()	# $1 = the XML temp file to check, $2 = name of the file
 		# Fix VDM filenames in location constants
 		BASE=$(basename $1)
 		sed -i -e "s+$BASE+$2+g" "$VDM"
+		
+		if [ -d model/Rules ]
+		then MODEL="model model/Rules"
+		else MODEL="model"
+		fi
 
 		java -Xmx1g -cp vdmcheck3.jar:vdmj.jar:annotations.jar com.fujitsu.vdmj.VDMJ \
 			-vdmsl -q -annotations -e "isValidFMIConfiguration($VAR)" \
-			model $VDM |
+			$MODEL $VDM |
 			awk '/^true$/{ print "No errors found."; exit 0 };/^false$/{ print "Errors found."; exit 1 };{ print }'
 	)
 	
